044d5a652d6a70d36e4843e146170cc7972dada8
Yeniamin <veniaminbabiy@gmail.com>
2020-05-01 17:59:30 +0300

    HW21

--- HW19.1/js/script.js
+++ HW19.1/js/script.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', function(){
 
     'use strict';
 
+    
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
