32aef97f14bac5e03564d5df1ee1a114f8a42d5c
Yeniamin <veniaminbabiy@gmail.com>
2020-05-04 17:47:13 +0300

    HW21

--- HW19.1/js/script.js
+++ HW19.1/js/script.js
@@ -36,4 +36,52 @@ window.addEventListener('DOMContentLoaded', function(){
         }
     });
 
+    // timer
+
+    let deadline = '2020-10-10';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) & 60),
+        hours = Math.floor((t/(1000*60*60))); 
+
+        return { 
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (minutes.textContent < 10) {
+                minutes.textContent = '0' + t.minutes;
+            }
+
+            if (seconds.textContent < 10) {
+                seconds.textContent = '0' + t.seconds;
+            }
+            
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock ('timer', deadline);
+
 });
--- HW21/script.js
+++ HW21/script.js
@@ -1,6 +1,15 @@
 let box = document.querySelector('.box'),
-    width = box.clientWidth,
-    height = box.clientHeight;
+    width = box.clienWidth,
+    height = box.clienHeight,
+    btn = document.querySelector('button');
 
 console.log(width);
 console.log(height);
+console.log(box.getBoundingClientRect());
+console.log(document.documentElement.clientWidth);
+
+console.log(document.documentElement.clientHeight);
+
+btn.addEventListener('click', function(){
+   box.scrollTop = 0;
+});
